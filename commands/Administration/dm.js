89620cdc51d23f79e70d559b3fba99d17d934cb0
Arijit Debnath <75007470+ArijitTheDev@users.noreply.github.com>
2022-03-11 21:00:43 +0530

    Update dm.js

--- commands/Administration/dm.js
+++ commands/Administration/dm.js
@@ -27,11 +27,6 @@ module.exports = {
   };
 
 
-/**
- * @INFO
- * Bot Coded by RogmitOp#6051 |
- * https://www.youtube.com/channel/UCPJRB-I9FCkWzgN3c2vKIpQ
- * @INFO
- * Please mention Him , when using this Code!
- * @INFO
- */
+
+
+
