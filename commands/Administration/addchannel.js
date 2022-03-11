3a847fca50b3cf3855bf2f4f69fd03e7be06ba91
Arijit Debnath <75007470+ArijitTheDev@users.noreply.github.com>
2022-03-11 22:11:49 +0530

    Update addchannel.js

--- commands/Administration/addchannel.js
+++ commands/Administration/addchannel.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 const config = require("../../botconfig/config.json");
 const ee = require("../../botconfig/embed.json");
-//const fs = require("fs")
+const fs = require("fs")
 module.exports = {
     name: "addchannel",
     category: "Setup",
