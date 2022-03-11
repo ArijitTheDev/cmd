6d3a640162cffc1d459e893fdb675fea870eb083
Arijit Debnath <75007470+ArijitTheDev@users.noreply.github.com>
2022-03-11 20:56:31 +0530

    Update dm.js

--- commands/Administration/dm.js
+++ commands/Administration/dm.js
@@ -15,14 +15,14 @@ module.exports = {
         message.guild.members.cache.get(args[0]);
       if (!user)
         return message.channel.send(
-          `<a:no:865963806483808256> You did not mention a user, or you gave an invalid id`
+          `You did not mention a user, or you gave an invalid id`
         );
       if (!args.slice(1).join(" "))
-        return message.channel.send("<a:no:865963806483808256> You did not specify your message");
+        return message.channel.send("You did not specify your message");
       user.user
         .send(args.slice(1).join(" "))
-        .catch(() => message.channel.send("<a:no:865963806483808256> That user could not be DMed!"))
-        .then(() => message.channel.send(`<a:yes:865963544380964864> Sent a message to ${user.user.tag}`));
+        .catch(() => message.channel.send("That user could not be DMed!"))
+        .then(() => message.channel.send(`Sent a message to ${user.user.tag}`));
     },
   };
 
